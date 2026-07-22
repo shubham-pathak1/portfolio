bcb05882fdfaa1068a4daa005ef64bad9827f8e3
shubham-pathak1 <shubhamxkcd@gmail.com>
2026-07-22 12:08:17 +0530

    updated gdrive resume link

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -192,7 +192,7 @@ export const About = () => {
                         {/* Mobile CTA */}
                         <div className="flex md:hidden items-center gap-3 w-full mb-10">
                             <a
-                                href="https://drive.google.com/file/d/1Wkf6o-0xPUCd4i9delwCCbdOFTkJnhM2/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1V9RuxDZsdsKOK5KVj6S5AwSXaZZ0P2m1/view?usp=sharing"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 aria-label="Open resume in a new tab"
@@ -226,7 +226,7 @@ export const About = () => {
                         <div className="hidden md:flex flex-wrap items-center justify-center md:justify-start gap-6">
                             <div className="flex items-center gap-3 flex-wrap justify-center">
                                 <a
-                                    href="https://drive.google.com/file/d/1Wkf6o-0xPUCd4i9delwCCbdOFTkJnhM2/view?usp=sharing"
+                                    href="https://drive.google.com/file/d/1V9RuxDZsdsKOK5KVj6S5AwSXaZZ0P2m1/view?usp=sharing"
                                     target="_blank"
                                     rel="noopener noreferrer"
                                     aria-label="Open resume in a new tab"
